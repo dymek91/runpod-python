e71361fead7f6919aa46f10cca7f37d4bb11f5b7
dymek91 <damian.barnas@gmail.com>
2023-12-27 10:38:59 +0100

    shorter sleep

--- runpod/serverless/modules/rp_scale.py
+++ runpod/serverless/modules/rp_scale.py
@@ -81,7 +81,7 @@ class JobScaler():
                 if job:
                     yield job
 
-            await asyncio.sleep(1)
+            await asyncio.sleep(0.1)
 
 
             log.debug(f"Concurrency set to: {self.current_concurrency}")
